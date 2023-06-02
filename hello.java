dbe5cb6cc4677acddb7ab768e563d71d48957202
mulaparthi <96194868+mulaparthi@users.noreply.github.com>
2023-06-02 20:06:24 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,9 +1,21 @@
 # frist-github-file
 package com.pardhu;
 
+class Employee{
+  int em_Id;
+  string name;
+  laxmi(int i,String s){
+    emp_Id=i;
+    name=s;
+    void display(){
+      System.out.println(emp_Id+" "+name);
+    }
+  }
 public class Java{
   public static void main(String[] args){
     System.out.println("This is your first java program..");
+    Laxmi l=new Laxmi(1,pardhu);
+    l.display()
   }
 }
   
